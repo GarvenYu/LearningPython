739e4d76766eda19d9b8cea19236a8dc46546571
ykbboom <1747603726@qq.com>
2018-02-05 23:51:25 +0800

    完成爬取第一个页面，将需要的数据存储到list中。

--- whut/pyspider/TaobaoGoods.py
+++ whut/pyspider/TaobaoGoods.py
@@ -26,15 +26,21 @@ def get_url_resource(url):
 def parse_html_text(text, infolist):
     re_title = re.compile(r'\"title\":\".*?\"')
     re_price = re.compile(r'\"price\":\"\d{3,5}\"')
-    titlelist = re_title.findall(text)
-    pricelist = re_price.findall(text)
+    title_list = re_title.findall(text)
+    price_list = re_price.findall(text)
+    for i in range(len(title_list)):
+        goods_title = title_list[i].split(':')[1].replace('"', '')
+        goods_price = price_list[i].split(':')[1].replace('"', '')
+        infolist.append(goods_title+':'+goods_price)
+    return infolist
 
 
 def main():
     mylist = []
     url = 'https://s.taobao.com/search?q=手机'
     text = get_url_resource(url)
-    parse_html_text(text, mylist)
+    info_list = parse_html_text(text, mylist)
+    print(info_list)
 
 
 main()
